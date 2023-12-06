6780fd7d731f02f200db194a5e691d16b11d5975
Janaina-Costa <janainacosta.dev@gmail.com>
2023-12-05 22:59:50 -0300

    feat: inicia implementação logica do register

--- src/components/Forms/RegisterForm/index.tsx
+++ src/components/Forms/RegisterForm/index.tsx
@@ -1,18 +1,68 @@
-import { useEffect, useRef } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from '../../Button';
 import { Divider } from '../../Divider';
 import Input from '../../Input';
 
-export const RegisterForm = () => {
+interface IProps {
+  value: {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    phone: string;
+    photoUrl: string;
+  };
+  onChangeInputName: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChangeInputEmail: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChangeInputPassword: (e: ChangeEvent<HTMLInputElement>) => void;
+
+  onChangeInputPhone: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChangeInputPhotoUrl: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmitRegister: (e: FormEvent) => void;
+}
+
+export const RegisterForm = ({
+  onChangeInputEmail,
+  onChangeInputName,
+  onChangeInputPassword,
+  onChangeInputPhone,
+  onChangeInputPhotoUrl,
+  onSubmitRegister,
+  value,
+}: IProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChangeInputName = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeInputName(e);
+  };
+  const handleChangeInputEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeInputEmail(e);
+  };
+  const handleChangeInputPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeInputPassword(e);
+  };
+  const handleChangeInputPhone = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeInputPhone(e);
+  };
+  const handleChangeInputPhotoUrl = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeInputPhotoUrl(e);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmitRegister(e);
+  };
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
   return (
-    <form className=" w-full flex flex-col justify-center items-center">
+    <form
+      className=" w-full flex flex-col justify-center items-center"
+      onSubmit={handleSubmit}
+    >
       <p className="text-center mt-4 ">
         Preencha os campos para criar o seu cadastro.
       </p>
@@ -22,22 +72,43 @@ export const RegisterForm = () => {
           placeholder="Digite seu nome"
           type="text"
           className="bg-transparent py-5 pl-4 w-full max-w-full rounded outline-none"
+          onChange={handleChangeInputName}
+          value={value.name}
+          name="name"
           ref={inputRef}
         />
         <Input
           placeholder="Digite seu e-mail"
           type="email"
           className="bg-transparent py-5 pl-4 w-full max-w-full rounded outline-none"
+          onChange={handleChangeInputEmail}
+          value={value.email}
+          name="email"
         />
         <Input
           placeholder="Digite sua senha"
           type="password"
           className="bg-transparent py-5 pl-4 w-full max-w-full rounded outline-none"
+          onChange={handleChangeInputPassword}
+          value={value.password}
+          name="password"
+        />
+
+        <Input
+          placeholder="Digite seu celular"
+          type="tel"
+          className="bg-transparent py-5 pl-4 w-full max-w-full rounded outline-none"
+          onChange={handleChangeInputPhone}
+          value={value.phone}
+          name="phone"
         />
         <Input
-          placeholder="Repita sua senha"
-          type="password"
+          placeholder="Digite a URL da sua foto de perfil"
+          type="text"
           className="bg-transparent py-5 pl-4 w-full max-w-full rounded outline-none"
+          onChange={handleChangeInputPhotoUrl}
+          value={value.photoUrl}
+          name="photoUrl"
         />
         <Button
           isSelected
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,44 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 import { RegisterForm } from '../../components/Forms/RegisterForm';
 
 export const Register = () => {
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone: '',
+    photoUrl: '',
+  });
+
+  const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitRegister = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+
   return (
     <div className="bg-gray-900 max-w-[40rem] m-auto mt-16 p-8 flex justify-center items-center max-md:mt-0 max-md:h-screen overflow-hidden">
-      <RegisterForm />
+      <RegisterForm
+        value={{
+          name: inputValue.name,
+          email: inputValue.email,
+          password: inputValue.password,
+          confirmPassword: inputValue.confirmPassword,
+          phone: inputValue.phone,
+          photoUrl: inputValue.photoUrl,
+        }}
+        onChangeInputName={handleChangeValue}
+        onChangeInputEmail={handleChangeValue}
+        onChangeInputPassword={handleChangeValue}
+        onChangeInputPhone={handleChangeValue}
+        onChangeInputPhotoUrl={handleChangeValue}
+        onSubmitRegister={handleSubmitRegister}
+      />
     </div>
   );
 };
